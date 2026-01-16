db0c84204866aba5f1b4269a68b8d2e92d801629
Andres <andresitomanuel@gmail.com>
2026-01-16 18:27:33 -0400

    Invalid command hardcoded output

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,18 @@
 const std = @import("std");
 
 pub fn main() !void {
+
     var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
     const stdout = &stdout_writer.interface;
-    // TODO: Uncomment the code below to pass the first stage
+    try stdout.print("$ ", .{});
+    
     var input_buffer: [4096]u8 = undefined;
     var input_reader = std.fs.File.stdin().readerStreaming(&input_buffer);
     const input_i = &input_reader.interface;
 
-    const command = try input_i.takeDelimiter('\n');
-    if (command) |command_value| {
-        try stdout.print("{s} command not found\n", .{command_value});
-    } else {
-        try stdout.print("Invalid input", .{});
+    if (try input_i.takeDelimiter('\n')) |cmd| {
+        const command = std.mem.trimRight(u8, cmd, "\r");
+        try stdout.print("{s}: command not found\n", .{command});
+        try stdout.flush();
     }
 }
