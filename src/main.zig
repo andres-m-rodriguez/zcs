85bad3b2f367cf507eb329214a6d9ad57e585ed0
Andres <andresitomanuel@gmail.com>
2026-01-16 18:16:41 -0400

    Invalid command hardcoded output

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,8 @@
 const std = @import("std");
 
-var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
-const stdout = &stdout_writer.interface;
-
 pub fn main() !void {
+    var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
+    const stdout = &stdout_writer.interface;
     // TODO: Uncomment the code below to pass the first stage
     var input_buffer: [4096]u8 = undefined;
     var input_reader = std.fs.File.stdin().readerStreaming(&input_buffer);
