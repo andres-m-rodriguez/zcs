7bb28935d654acac9d0ad50ac470614e77095d80
Andres <andresitomanuel@gmail.com>
2026-01-16 18:37:35 -0400

    Add exit command

--- src/main.zig
+++ src/main.zig
@@ -7,13 +7,17 @@ pub fn main() !void {
     var input_buffer: [4096]u8 = undefined;
     var input_reader = std.fs.File.stdin().readerStreaming(&input_buffer);
     const input_i = &input_reader.interface;
-    while (true) {
+    var is_running = true;
+    while (is_running) {
         try stdout.print("$ ", .{});
+        const command_input = try input_i.takeDelimiter('\n') orelse "";
 
-        if (try input_i.takeDelimiter('\n')) |cmd| {
-            const command = std.mem.trimRight(u8, cmd, "\r");
-            try stdout.print("{s}: command not found\n", .{command});
-            try stdout.flush();
+        const command = std.mem.trimRight(u8, command_input, "\r");
+        try stdout.print("{s}: command not found\n", .{command});
+        try stdout.flush();
+
+        if(std.mem.eql(u8,command, "exit")){
+            is_running = false;
         }
     }
 }
